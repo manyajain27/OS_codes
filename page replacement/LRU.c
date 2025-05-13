b09bd74d8163776dd803b117c9facdc98d05a755
manyajain27 <jainmanya2701@gmail.com>
2025-05-13 16:24:05 +0530

    lru and optimal

--- page replacement/LRU.c
+++ page replacement/LRU.c
@@ -1,100 +1,91 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int findLRU(int time[], int f) {
-    int min = time[0], pos = 0;
-    for(int i = 1; i < f; i++) {
-        if(time[i] < min) {
-            min = time[i];
-            pos = i;
-        }
-    }
-    return pos;
-}
-
-int main() {
-    int pages[100], frames[100][100], memory[100], time[100];
-    int n, f, i, j, counter = 0;
-    int pageFaults = 0, pageHits = 0;
-
+int main()
+{
+    int n, f;
     printf("Enter number of pages: ");
     scanf("%d", &n);
 
-    printf("Enter page reference string: ");
-    for(i = 0; i < n; i++) {
+    int pages[n];
+    printf("Enter reference string:\n");
+    for (int i = 0; i < n; i++)
+    {
         scanf("%d", &pages[i]);
     }
 
     printf("Enter number of frames: ");
     scanf("%d", &f);
 
-    for(i = 0; i < f; i++) {
+    int memory[f];
+    int last_used[f]; // to store when each frame was last used
+    int page_faults = 0;
+
+    // initialize
+    for (int i = 0; i < f; i++)
+    {
         memory[i] = -1;
-        time[i] = 0;
+        last_used[i] = -1;
     }
 
-    for(i = 0; i < n; i++) {
-        int hit = 0;
+    printf("\nPage\tFrames\t\tPage Fault\n");
 
-        for(j = 0; j < f; j++) {
-            if(memory[j] == pages[i]) {
-                hit = 1;
-                pageHits++;
-                counter++;
-                time[j] = counter; // update recent use
+    for (int i = 0; i < n; i++)
+    {
+        int page = pages[i];
+        bool hit = false;
+
+        // check for hit
+        for (int j = 0; j < f; j++)
+        {
+            if (memory[j] == page)
+            {
+                hit = true;
+                last_used[j] = i; // update last used time
                 break;
             }
         }
 
-        if(!hit) {
-            int pos = -1;
+        if (!hit)
+        {
+            int replace_index = -1;
+            int lru_time = 9999;
 
-            for(j = 0; j < f; j++) {
-                if(memory[j] == -1) {
-                    pos = j;
+            // find either empty or least recently used
+            for (int j = 0; j < f; j++)
+            {
+                if (memory[j] == -1)
+                {
+                    replace_index = j;
                     break;
+                }
+                else if (last_used[j] < lru_time)
+                {
+                    lru_time = last_used[j];
+                    replace_index = j;
                 }
             }
 
-            if(pos == -1) {
-                pos = findLRU(time, f);
-            }
-
-            memory[pos] = pages[i];
-            counter++;
-            time[pos] = counter;
-            pageFaults++;
+            memory[replace_index] = page;
+            last_used[replace_index] = i;
+            page_faults++;
         }
 
-        for(j = 0; j < f; j++) {
-            frames[j][i] = memory[j];
-        }
-    }
-
-    // Horizontal Output
-    printf("\nPage Reference String:\n");
-    for(i = 0; i < n; i++) {
-        printf(" %2d ", pages[i]);
-    }
-
-    printf("\n");
-    for(i = f - 1; i >= 0; i--) {
-        printf("F%d |", i + 1);
-        for(j = 0; j < n; j++) {
-            if(frames[i][j] == -1)
-                printf(" --");
+        // print current frame state
+        printf("%d\t", page);
+        for (int j = 0; j < f; j++)
+        {
+            if (memory[j] != -1)
+                printf("%d ", memory[j]);
             else
-                printf(" %2d", frames[i][j]);
+                printf("- ");
         }
-        printf("\n");
+
+        printf("\t\t%s\n", hit ? "Hit" : "Fault");
     }
 
-    float hitRatio = (float)pageHits / n;
-    float missRatio = (float)pageFaults / n;
-
-    printf("\nPage Hits: %d\n", pageHits);
-    printf("Page Faults: %d\n", pageFaults);
-    printf("Hit Ratio: %d/%d = %.2f\n", pageHits, n, hitRatio);
-    printf("Miss Ratio: %d/%d = %.2f\n", pageFaults, n, missRatio);
+    printf("\nTotal Page Faults = %d\n", page_faults);
+    printf("Total Page Hits = %d\n", n - page_faults);
 
     return 0;
 }
--- page replacement/optimal.c
+++ page replacement/optimal.c
@@ -1,104 +1,98 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int findOptimal(int pages[], int memory[], int n, int index, int f) {
-    int farthest = -1, pos = -1;
-    for(int i = 0; i < f; i++) {
-        int j;
-        for(j = index + 1; j < n; j++) {
-            if(memory[i] == pages[j]) {
-                if(j > farthest) {
-                    farthest = j;
-                    pos = i;
-                }
-                break;
-            }
-        }
-        // If not found in future, return immediately
-        if(j == n)
-            return i;
-    }
-    return (pos == -1) ? 0 : pos;
-}
-
-int main() {
-    int pages[100], frames[100][100], memory[100];
-    int n, f, i, j, k;
-    int pageFaults = 0, pageHits = 0;
-
+int main()
+{
+    int n, f;
     printf("Enter number of pages: ");
     scanf("%d", &n);
 
-    printf("Enter page reference string: ");
-    for(i = 0; i < n; i++) {
+    int pages[n];
+    printf("Enter reference string:\n");
+    for (int i = 0; i < n; i++)
+    {
         scanf("%d", &pages[i]);
     }
 
     printf("Enter number of frames: ");
     scanf("%d", &f);
 
-    for(i = 0; i < f; i++) {
+    int memory[f];
+    int page_faults = 0;
+
+    // init memory to -1
+    for (int i = 0; i < f; i++)
         memory[i] = -1;
-    }
 
-    for(i = 0; i < n; i++) {
-        int hit = 0;
+    printf("\nPage\tFrames\t\tPage Fault\n");
 
-        for(j = 0; j < f; j++) {
-            if(memory[j] == pages[i]) {
-                hit = 1;
-                pageHits++;
+    for (int i = 0; i < n; i++)
+    {
+        int page = pages[i];
+        bool hit = false;
+
+        // check for hit
+        for (int j = 0; j < f; j++)
+        {
+            if (memory[j] == page)
+            {
+                hit = true;
                 break;
             }
         }
 
-        if(!hit) {
-            int replacedIndex;
+        // if not a hit, replace using optimal logic
+        if (!hit)
+        {
+            int replace_index = -1, farthest = -1;
 
-            for(j = 0; j < f; j++) {
-                if(memory[j] == -1) {
-                    replacedIndex = j;
+            for (int j = 0; j < f; j++)
+            {
+                if (memory[j] == -1)
+                {
+                    replace_index = j;
                     break;
+                }
+
+                // find next use of memory[j]
+                int k;
+                for (k = i + 1; k < n; k++)
+                {
+                    if (pages[k] == memory[j])
+                        break;
+                }
+
+                if (k == n)
+                {
+                    // not used again
+                    replace_index = j;
+                    break;
+                }
+                else if (k > farthest)
+                {
+                    farthest = k;
+                    replace_index = j;
                 }
             }
 
-            if(j == f) {
-                replacedIndex = findOptimal(pages, memory, n, i, f);
-            }
-
-            memory[replacedIndex] = pages[i];
-            pageFaults++;
+            memory[replace_index] = page;
+            page_faults++;
         }
 
-        for(j = 0; j < f; j++) {
-            frames[j][i] = memory[j];
-        }
-    }
-
-    // Horizontal Table Print
-    printf("\nPage Reference String:\n");
-    for(i = 0; i < n; i++) {
-        printf(" %2d ", pages[i]);
-    }
-
-    printf("\n");
-    for(i = f - 1; i >= 0; i--) {
-        printf("F%d |", i + 1);
-        for(j = 0; j < n; j++) {
-            if(frames[i][j] == -1)
-                printf(" --");
+        // print output
+        printf("%d\t", page);
+        for (int j = 0; j < f; j++)
+        {
+            if (memory[j] != -1)
+                printf("%d ", memory[j]);
             else
-                printf(" %2d", frames[i][j]);
+                printf("- ");
         }
-        printf("\n");
+        printf("\t\t%s\n", hit ? "Hit" : "Fault");
     }
 
-    float hitRatio = (float)pageHits / n;
-    float missRatio = (float)pageFaults / n;
-
-    printf("\nPage Hits: %d\n", pageHits);
-    printf("Page Faults: %d\n", pageFaults);
-    printf("Hit Ratio: %d/%d = %.2f\n", pageHits, n, hitRatio);
-    printf("Miss Ratio: %d/%d = %.2f\n", pageFaults, n, missRatio);
+    printf("\nTotal Page Faults = %d\n", page_faults);
+    printf("Total Page Hits = %d\n", (n - page_faults));
 
     return 0;
 }
