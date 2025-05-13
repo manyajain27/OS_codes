a6d8b860856794c2ce86ea46733fd489cae8456c
manyajain27 <jainmanya2701@gmail.com>
2025-05-13 16:09:35 +0530

    fifo page replacement

--- page replacement/fifo.c
+++ page replacement/fifo.c
@@ -1,75 +1,67 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int main() {
-    int pages[100], frames[100][100], n, f, i, j, k = 0;
-    int pageFaults = 0, pageHits = 0;
-
-    printf("Enter number of pages: ");
+int main()
+{
+    int n, f;
+    printf("enter no. of pages: ");
     scanf("%d", &n);
 
-    printf("Enter page reference string: ");
-    for(i = 0; i < n; i++) {
+    int pages[n];
+
+    printf("\nenter reference string:\n");
+    for (int i = 0; i < n; i++)
+    {
         scanf("%d", &pages[i]);
     }
 
-    printf("Enter number of frames: ");
+    printf("\nenter no. of frames: ");
     scanf("%d", &f);
 
     int memory[f];
-    for(i = 0; i < f; i++) {
+    int front = 0; // initialising the first value to be replaced
+    int page_faults = 0, page_hits = 0;
+
+    // initialise memory with -1
+    for (int i = 0; i < f; i++)
+    {
         memory[i] = -1;
     }
 
-    for(i = 0; i < n; i++) {
+    printf("\nPage\tFrames\t\tPage Fault\n");
+    for (int i = 0; i < n; i++)
+    {
         int hit = 0;
-
-        // Check for hit
-        for(j = 0; j < f; j++) {
-            if(memory[j] == pages[i]) {
+        for (int j = 0; j < f; j++)
+        {
+            if (memory[j] == pages[i])
+            {
                 hit = 1;
-                pageHits++;
                 break;
             }
         }
 
-        // If miss, replace using FIFO
-        if(hit == 0) {
-            memory[k] = pages[i];
-            k = (k + 1) % f;
-            pageFaults++;
+        if (hit == 0)
+        {
+            memory[front] = pages[i];
+            front = (front + 1) % f; // circular to keep fifo fashion
+            page_faults++;
         }
 
-        // Store current memory state
-        for(j = 0; j < f; j++) {
-            frames[j][i] = memory[j];
-        }
-    }
-
-    // Print horizontal table
-    printf("\nPage Reference String:\n");
-    for(i = 0; i < n; i++) {
-        printf(" %2d ", pages[i]);
-    }
-
-    printf("\n");
-    for(i = f - 1; i >= 0; i--) {
-        printf("F%d |", i + 1);
-        for(j = 0; j < n; j++) {
-            if(frames[i][j] == -1)
-                printf(" --");
+        printf("%d\t", pages[i]);
+        for (int j = 0; j < f; j++)
+        {
+            if (memory[j] != -1)
+                printf("%d ", memory[j]);
             else
-                printf(" %2d", frames[i][j]);
+                printf("- ");
         }
-        printf("\n");
+
+        printf("\t\t%s\n", hit ? "Hit" : "Fault");
     }
 
-    float hitRatio = (float)pageHits / n;
-    float missRatio = (float)pageFaults / n;
-
-    printf("\nPage Hits: %d\n", pageHits);
-    printf("Page Faults: %d\n", pageFaults);
-    printf("Hit Ratio: %d/%d = %.2f\n", pageHits, n, hitRatio);
-    printf("Miss Ratio: %d/%d = %.2f\n", pageFaults, n, missRatio);
+    printf("\nTotal Page Faults = %d\n", page_faults);
+    printf("\nTotal Page Hits = %d\n", (n - page_faults));
 
     return 0;
 }
